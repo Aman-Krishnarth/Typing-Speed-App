e10e0bc57e8f23a29d660b64951cff2adf2a01aa
Aman <20031010.aman@gmail.com>
2024-09-18 01:52:08 +0530

    Added loading screen

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -10,6 +10,8 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [showLoading, setShowLoading] = useState(true);
+
   function handleCreateUser(e) {
     e.preventDefault();
     navigate("/create");
@@ -29,7 +31,7 @@ function Login() {
           password,
         })
         .then((res) => {
-          console.log("BACKEND RUNNING")
+          setShowLoading(false)
         })
         .catch((err) => {
           console.log("LOGIN POST ERROR");
@@ -98,51 +100,85 @@ function Login() {
   }
 
   return (
-    <div className=" h-lvh flex flex-col ">
-      <form
-        onSubmit={handleFormSubmit}
-        className="w-3/4 min-h-3/4 m-auto items-stretch rounded-md flex flex-col p-4 gap-2 "
-      >
-        <div className="flex  justify-around items-center p-3 my-4 rounded-xl bg-[#121315]">
-          <input
-            type="text"
-            placeholder="Tap to Add Email"
-            className="outline-none bg-transparent text-white text-2xl text-center bg-blue-400 w-full h-full text-wrap"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
+    <>
+      {showLoading ? (
+        <div class="flex h-screen w-screen items-center justify-center">
+          <button
+            type="button"
+            class="flex items-center rounded-lg bg-green-700 px-4 py-2 text-white"
+            disabled
+          >
+            <svg
+              class="mr-3 h-5 w-5 animate-spin text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span class="font-medium"> Processing... </span>
+          </button>
         </div>
+      ) : (
+        <div className=" h-lvh flex flex-col ">
+          <form
+            onSubmit={handleFormSubmit}
+            className="w-3/4 min-h-3/4 m-auto items-stretch rounded-md flex flex-col p-4 gap-2 "
+          >
+            <div className="flex  justify-around items-center p-3 my-4 rounded-xl bg-[#121315]">
+              <input
+                type="text"
+                placeholder="Tap to Add Email"
+                className="outline-none bg-transparent text-white text-2xl text-center bg-blue-400 w-full h-full text-wrap"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
 
-        <div className="flex  justify-around items-center p-3 my-4 rounded-xl bg-[#121315]">
-          <input
-            type="password"
-            placeholder="Password"
-            className="outline-none bg-transparent text-white text-2xl text-center bg-blue-400 w-full h-full"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </div>
+            <div className="flex  justify-around items-center p-3 my-4 rounded-xl bg-[#121315]">
+              <input
+                type="password"
+                placeholder="Password"
+                className="outline-none bg-transparent text-white text-2xl text-center bg-blue-400 w-full h-full"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
 
-        <input
-          type="submit"
-          value="Login"
-          className="bg-[#121315] m-auto
+            <input
+              type="submit"
+              value="Login"
+              className="bg-[#121315] m-auto
 			p-4 rounded-xl hover:scale-110 duration-300 ease-in-out cursor-pointer text-3xl font-semibold hover:px-6 my-4 text-white hover:text-green-500"
-        />
+            />
 
-        <input
-          type="button"
-          value="Not a user? Register Now!"
-          className=" text-white mx-auto my-7
+            <input
+              type="button"
+              value="Not a user? Register Now!"
+              className=" text-white mx-auto my-7
 			p-4 rounded-xl hover:scale-110 duration-300 ease-in-out cursor-pointer text-3xl font-semibold hover:px-6 bg-[#121315] text-wrap hover:text-green-500"
-          onClick={handleCreateUser}
-        />
-      </form>
-    </div>
+              onClick={handleCreateUser}
+            />
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
